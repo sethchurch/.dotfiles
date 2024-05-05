f075f28d71e0efd4ea6099b561d6755e80646095
sethchurch <sethcdev@gmail.com>
2024-05-05 19:48:03 -0400

    neovim config

---
+++ neovim/.config/nvim/lua/custom/plugins/git.lua
@@ -0,0 +1,13 @@
+-- NeoGit
+-- https://github.com/NeogitOrg/neogit
+
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+    'nvim-telescope/telescope.nvim',
+    'ibhagwan/fzf-lua',
+  },
+  config = true,
+}
---
+++ neovim/.config/nvim/lua/custom/plugins/typescript-tools.lua
@@ -0,0 +1,8 @@
+-- Typescript tools and LSP
+-- https://github.com/pmizio/typescript-tools.nvim
+
+return {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+}
