37ba716a7de29404a83e3dc0e25cb1dc745f1114
Seth Church <sethcdev@gmail.com>
2024-05-10 19:19:58 -0400

    reconfig and organize gitbased commands

--- neovim/.config/nvim/lua/plugins/gitdiff.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'sindrets/diffview.nvim',
-  config = function()
-    require('diffview').setup()
-    vim.keymap.set('n', '<leader>ndd', '<cmd>:DiffviewFileHistory %<CR>', { desc = 'Open [D]iff View' })
-    vim.keymap.set('n', '<leader>ndc', '<cmd>:DiffviewClose<CR>', { desc = 'Close [D]iff View' })
-  end,
-}
