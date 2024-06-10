c00461d7d320d3732daf88b8d74ae3c0589f66e9
Seth Church <sethcdev@gmail.com>
2024-06-10 09:04:17 -0400

    adjust lualine

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -38,7 +38,11 @@ return {
         -- section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_z = { "" },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = { "filetype" },
       },
     },
   },
