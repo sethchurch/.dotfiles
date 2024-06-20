4b7c7734225863613571aee521e5c39af83c360c
Seth Church <sethcdev@gmail.com>
2024-06-19 20:17:39 -0400

    cleanup lualine setup

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -27,15 +27,6 @@ return {
       options = {
         component_separators = "",
         section_separators = "",
-
-        -- component_separators = "",
-        -- section_separators = { left = "", right = "" },
-
-        -- component_separators = { left = "", right = "" },
-        -- section_separators = { left = "", right = "" },
-
-        -- component_separators = "",
-        -- section_separators = { left = "", right = "" },
       },
       sections = {
         lualine_y = {
